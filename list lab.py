353ea20923cb7182424d936989e8f9cc75f85b2c
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2016-12-01 00:30:36 +0100

    Update list lab.py

--- list lab.py
+++ list lab.py
@@ -4,7 +4,6 @@ def manipulate_data(lists):
       negative_list = filter(lambda x: x < 0, lists)
       return [len(positive_list), sum(negative_list)]
 
-print manipulate_data((0,1,2,5,-2,-4,0,-1))
 
 
     
